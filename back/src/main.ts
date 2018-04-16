e560f6bd3ab338a55c4506450f13873468f993e3
Hougo13 <hougo13@gmail.com>
2018-04-16 11:32:10 +0200

    cors

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import { GroupsGuard } from './groups.guard';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(cors());
   const groupsGuard = app.select(AppModule).get(GroupsGuard);
   app.useGlobalGuards(groupsGuard);
   await app.listen(3000);
