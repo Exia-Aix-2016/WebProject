f03c4e2b42d0e1a403389516e0afea61b06a4e26
Hougo13 <hougo13@gmail.com>
2018-04-17 16:15:43 +0200

    rm express init
    rm cors import

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from '@nestjs/core';
-import * as express from 'express';
-import { raw } from 'body-parser';
 import { AppModule } from './app/app.module';
 import { GroupsGuard } from './groups.guard';
-import * as cors from 'cors';
 
 async function bootstrap() {
-  const server = express();
-  server.use(raw({ limit: '200kb' }));
-  const app = await NestFactory.create(AppModule, server, { cors: true });
+  const app = await NestFactory.create(AppModule, { cors: true });
   const groupsGuard = app.select(AppModule).get(GroupsGuard);
   app.useGlobalGuards(groupsGuard);
   await app.listen(3000);
