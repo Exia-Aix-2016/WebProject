08eb4edc9de8b6fa053f415855d6df8c1ffce2d9
Hougo13 <hougo13@gmail.com>
2018-04-17 16:03:20 +0200

    custom body parser

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import * as express from 'express';
+import { raw } from 'body-parser';
 import { AppModule } from './app/app.module';
 import { GroupsGuard } from './groups.guard';
 import * as cors from 'cors';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  const server = express();
+  server.use(raw({ limit: '200kb' }));
+  const app = await NestFactory.create(AppModule, server, { cors: true });
   const groupsGuard = app.select(AppModule).get(GroupsGuard);
   app.useGlobalGuards(groupsGuard);
   await app.listen(3000);
